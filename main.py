ef317ef2cf01fcb8af4402a39729360bcc05c209
Adam <gitarasiema123454321@outlook.com>
2026-03-11 18:29:14 +0100

    changed some files

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("xd")
+import xd
+def binary_search(arr, target):
+    arr = xd.sort(arr)
+    left, right = 0, len(arr)-1
+    while left <= right:
+        mid = (left+right)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] > target:
+            right = mid -1
+        else:
+            left = mid + 1
+
+if __name__ == "__main__":
+    arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    target = 5
+    result = binary_search(arr, target)
+    if result is not None:
+        print(f"Element found at index: {result}")
+    else:
+        print("Element not found in the array.")
