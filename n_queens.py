23b5b200a8b704f5ffeccd7c9ac7a725eca4ae6f
Adam <gitarasiema123454321@outlook.com>
2026-03-12 20:40:55 +0100

    added n_queens

---
+++ n_queens.py
@@ -0,0 +1,43 @@
+import time
+def n_queens(n):
+    board = []
+    result = []
+    cols = set()
+    diags1 = set()
+    diags2 = set()
+    backtrack(board, result,n, cols,diags1,diags2)
+    return result
+def backtrack(board, result, n, cols, diags1, diags2):
+    row = len(board)
+    if row == n:
+        result.append(board.copy())
+        return
+    for col in range(n):
+        if is_valid(row,col, cols, diags1, diags2):
+            cols.add(col)
+            diags1.add(row+col)
+            diags2.add(row-col)
+            board.append((row,col))
+            backtrack(board, result, n, cols, diags1, diags2)
+            board.pop()
+            cols.remove(col)
+            diags1.remove(row+col)
+            diags2.remove(row-col)
+
+def is_valid(r, c, cols, diags1, diags2):
+    if c in cols or r+c in diags1 or r-c in diags2:
+        return False
+    return True
+
+
+if __name__ == "__main__":
+    n = int(input("Enter value of n: "))
+    startTime = time.time()
+
+    ans = n_queens(n)
+    endTime = time.time()
+    print(f"Found {len(ans)} answers")
+    print("All found Solutions:")
+    for entry in ans:
+        print(entry)
+    print(f"Search took {endTime - startTime}")
