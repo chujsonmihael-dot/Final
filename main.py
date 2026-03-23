9e6d4af9448d4fb107a2a000bb53b7e881c1bb75
Adam <gitarasiema123454321@outlook.com>
2026-03-23 20:24:32 +0100

    started implemnting window gui

--- main.py
+++ main.py
@@ -40,14 +40,24 @@ def MuliToolMainConsole():
             print("------------------------------")
 
 def MultiToolMainWindow():
-    root = tk.TK()
-    root.title("Multi-Tool v0.1")
+    display = tk.Tk()
+    display.title("Multi-Tool v0.1")
+    display.geometry("640x360") # SIZE
+    display.configure(bg="#3d3d3d")
 
-    root.mainloop()
+    MainTitle1 = tk.Label(display, text="Welcome to", font=("Arial", 24, "bold"), bg="#3d3d3d",fg="#d4d4d4")
+    MainTitle2 = tk.Label(display, text="Multi-Tool v0.1", font=("Arial", 32, "bold"), bg="#3d3d3d",fg="#d4d4d4")
+
+    MainTitle1.place(relx=0.5,rely=0.05,anchor="center")
+    MainTitle2.place(relx=0.5, rely=0.175, anchor="center")
+
+
+    display.mainloop()
 
 if __name__ == "__main__":
-    MuliToolMainConsole()
     MultiToolMainWindow()
+    
+    
     
     
     
