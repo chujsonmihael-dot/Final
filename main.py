9551091d6973b993af4086f69bfc74de0aa7b97c
Adam <gitarasiema123454321@outlook.com>
2026-03-24 21:27:45 +0100

    moved test file to a more appropriate location

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from programs import binary_search, DFS, Encrypter, fibonacci_numbers, n_queens
 
 from complementary import utils, array_funcs
 
-import tkinter as tk
+
 
 tools = {1:"Binary Search",
          2:"Depth First Search",
@@ -39,33 +39,7 @@ def MuliToolMainConsole():
             print("Error!")
             print("------------------------------")
 
-def MultiToolMainWindow():
-    display = tk.Tk()
-    display.title("Multi-Tool v0.1")
-    display.geometry("640x360") # SIZE
-    display.configure(bg="#3d3d3d")
 
-    MainTitle1 = tk.Label(display, text="Welcome to", font=("Arial", 24, "bold"), bg="#3d3d3d",fg="#d4d4d4")
-    MainTitle2 = tk.Label(display, text="Multi-Tool v0.1", font=("Arial", 32, "bold"), bg="#3d3d3d",fg="#d4d4d4")
-
-    MainTitle1.place(relx=0.5,rely=0.05,anchor="center")
-    MainTitle2.place(relx=0.5, rely=0.175, anchor="center")
-
-    def ButtonClick(key):
-        print("xd")
-
-    #ADDNS BUTTONS
-    posy = 0.3
-    posx = 0.025
-    for key in tools:
-        NewButtonText = tk.Button(display, text=tools[key], font=("Sans-Serif", 14), command=lambda k = key: ButtonClick(key))
-        NewButtonText.place(anchor="w", relx=posx,rely=posy )
-        posy+=0.125
-
-    display.mainloop()
-
-if __name__ == "__main__":
-    MultiToolMainWindow()
     
     
     
